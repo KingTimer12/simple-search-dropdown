ede26e3e95fd772579ec0dd2ea364b62c2733f38
kingtimer12 <queentimer12@gmail.com>
2024-08-22 15:38:03 -0300

    fix: Bug no update do componente

--- update.js
+++ update.js
@@ -1,33 +1,33 @@
-import fs from 'fs';
+import fs from 'fs'
 
-(() => {
+;(() => {
   const replaceVersion = (path) => {
-    let content = fs.readFileSync(path, 'utf-8');
-    const versionRegex = /"version":\s*"(\d+)\.(\d+)\.(\d+)"/;
+    let content = fs.readFileSync(path, 'utf-8')
+    const versionRegex = /"version":\s*"(\d+)\.(\d+)\.(\d+)"/
 
-    const match = content.match(versionRegex);
+    const match = content.match(versionRegex)
     if (match) {
-      let [fullMatch, major, minor, patch] = match;
-      let majorNum = parseInt(major);
-      let minorNum = parseInt(minor);
-      let patchNum = parseInt(patch);
-      patchNum++;
-      if (patchNum >= 10) {
-        patchNum = 0;
-        minorNum++;
+      let [fullMatch, major, minor, patch] = match
+      let majorNum = parseInt(major)
+      let minorNum = parseInt(minor)
+      let patchNum = parseInt(patch)
+      patchNum++
+      if (patchNum >= 100) {
+        patchNum = 0
+        minorNum++
       }
-      if (minorNum >= 10) {
-        minorNum = 0;
-        majorNum++;
+      if (minorNum >= 100) {
+        minorNum = 0
+        majorNum++
       }
-      const newVersion = `${majorNum}.${minorNum}.${patchNum}`;
-      const updatedContent = content.replace(fullMatch, `"version": "${newVersion}"`);
-      fs.writeFileSync(path, updatedContent);
-      console.log(`"${path}" version updated to ${newVersion}`);
+      const newVersion = `${majorNum}.${minorNum}.${patchNum}`
+      const updatedContent = content.replace(fullMatch, `"version": "${newVersion}"`)
+      fs.writeFileSync(path, updatedContent)
+      console.log(`"${path}" version updated to ${newVersion}`)
     } else {
-      console.log(`Version not found in "${path}"`);
+      console.log(`Version not found in "${path}"`)
     }
-  };
+  }
 
-  replaceVersion('./package.json');
-})();
+  replaceVersion('./package.json')
+})()
